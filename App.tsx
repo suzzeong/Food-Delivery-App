a630b522e864e914d4a449c133baffea3e6b43f4
suzzeong <su981006@naver.com>
2023-01-16 23:31:45 +0900

    test: flex와 요소 배치

--- App.tsx
+++ App.tsx
@@ -4,7 +4,16 @@ import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
-import {Text, TouchableHighlight, View} from 'react-native';
+import {
+  // onClick X, onPress O
+  Pressable, // 화면이 커지면서 페이지 이동
+  Button,
+  TouchableNativeFeedback,
+  Text,
+  TouchableHighlight, // 글씨가 검은색(underlayColor)이 되면서 페이지 이동
+  TouchableOpacity, // 글씨가 희미해지면서 페이지 이동
+  View,
+} from 'react-native';
 import {useCallback} from 'react';
 
 type RootStackParamList = {
@@ -20,11 +29,28 @@ function HomeScreen({navigation}: HomeScreenProps) {
   }, [navigation]);
 
   return (
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <TouchableHighlight onPress={onClick}>
-              <Text>Home Screen</Text>
-            </TouchableHighlight>
-          </View>
+    <>
+      <View
+        style={{
+          flex: 2,
+          backgroundColor: 'yellow',
+          justifyContent: 'flex-end', // 웹: alignItems
+          alignItems: 'center', // 웹: justifyContent
+        }}>
+        <Pressable
+          onPress={onClick}
+          style={{
+            backgroundColor: 'blue',
+            paddingHorizontal: 40,  // 좌우
+            paddingVertical: 20,  // 상하
+          }}>
+          <Text style={{color: 'white'}}>Home Screen</Text>
+        </Pressable>
+      </View>
+      <View style={{flex: 5, backgroundColor: 'orange'}}>
+        <Text>Second</Text>
+      </View>
+    </>
   );
 }
 
@@ -34,27 +60,32 @@ function DetailsScreen({navigation}: DetailsScreenProps) {
   }, [navigation]);
 
   return (
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <TouchableHighlight onPress={onClick}>
-              <Text>Details Screen</Text>
-            </TouchableHighlight>
-          </View>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <TouchableHighlight onPress={onClick}>
+        <Text>Details Screen</Text>
+      </TouchableHighlight>
+    </View>
   );
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 function App() {
   return (
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-              <Stack.Screen
-                      name="Home"
-                      component={HomeScreen}
-                      options={{title: '홈화면', headerShown: false}}
-              />
-              <Stack.Screen name="Details" component={DetailsScreen} />
-            </Stack.Navigator>
-          </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: '홈화면'}}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
