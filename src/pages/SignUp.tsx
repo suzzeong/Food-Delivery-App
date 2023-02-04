56c9f52057a9b1140042912e36e90e16112811ae
suzzeong <su981006@naver.com>
2023-02-04 23:28:21 +0900

    feat: axios로 서버 요청보내기

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -11,6 +11,7 @@ import {
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../AppInner';
 import DismissKeyboardView from '../components/DismissKeyboardView';
+import axios from 'axios';
 
 type SignUpScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
@@ -31,7 +32,7 @@ function SignUp({navigation}: SignUpScreenProps) {
   const onChangePassword = useCallback((text: string) => {
     setPassword(text.trim());
   }, []);
-  const onSubmit = useCallback(() => {
+  const onSubmit = useCallback(async () => {
     if (!email || !email.trim()) {
       return Alert.alert('알림', '이메일을 입력해주세요.');
     }
@@ -55,6 +56,14 @@ function SignUp({navigation}: SignUpScreenProps) {
       );
     }
     console.log(email, name, password);
+    try {
+      // axios(http 메서드) - post, get, put, patch, delete, head, options
+      const response = await axios.post('/user', {email, name, password});
+      console.log(response);
+    } catch (error) {
+      // console.log(error);
+    } finally {
+    }
     Alert.alert('알림', '회원가입 되었습니다.');
   }, [email, name, password]);
 
