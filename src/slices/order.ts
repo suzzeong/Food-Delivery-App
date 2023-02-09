05e244314ca4015be9f7d6bdee47a506a2ec454e
suzzeong <su981006@naver.com>
2023-02-09 17:24:36 +0900

    feat: about redux

---
+++ src/slices/order.ts
@@ -0,0 +1,13 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {
+  },
+  extraReducers: builder => {},
+});
+
+export default orderSlice;
--- src/slices/user.ts
+++ src/slices/user.ts
@@ -1,15 +1,23 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+// store -> reducer(state) -> user slice, order slice
+// ex) state.user.email, state.order, state.ui.loading
+
+// action: state를 바꾸는 행위/동작
+// dispatch: 그 액션을 실제로 실행하는 함수
+// reducer: 액션이 실제로 실행되면 state를 바꾸는 로직
+
 const initialState = {
   name: '',
   email: '',
   accessToken: '',
   refreshToken: '',
 };
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {
+  reducers: { // 동기 액션
     setUser(state, action) {
       state.email = action.payload.email;
       state.name = action.payload.name;
@@ -17,7 +25,9 @@ const userSlice = createSlice({
       state.refreshToken = action.payload.refreshToken;
     },
   },
-  extraReducers: builder => {},
+  extraReducers: builder => { // 비동기 액션
+
+  },
 });
 
 export default userSlice;
