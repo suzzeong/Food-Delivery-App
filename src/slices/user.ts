24853d8e19203b9793e3f38345c217a3030e52f8
suzzeong <su981006@naver.com>
2023-02-09 17:08:38 +0900

    fix: refreshToken 추가

--- src/slices/user.ts
+++ src/slices/user.ts
@@ -14,6 +14,7 @@ const userSlice = createSlice({
       state.email = action.payload.email;
       state.name = action.payload.name;
       state.accessToken = action.payload.accessToken;
+      state.refreshToken = action.payload.refreshToken;
     },
   },
   extraReducers: builder => {},
