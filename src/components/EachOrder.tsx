af2713113d6b5532d68a6d2d887ddad12532ca8a
suzzeong <su981006@naver.com>
2023-02-11 22:46:42 +0900

    feat: FlatList 파일 분리

---
+++ src/components/EachOrder.tsx
@@ -0,0 +1,58 @@
+import {useCallback} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {Order} from '../slices/order';
+
+function EachOrder({item}: {item: Order}) {
+  const toggleDetail = useCallback(() => {}, []);
+
+  return (
+    <View style={styles.orderContainer}>
+      <Pressable onPress={toggleDetail} style={styles.info}>
+        <Text style={styles.eachInfo}>
+          {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+        </Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  orderContainer: {
+    borderRadius: 5,
+    margin: 5,
+    padding: 10,
+    backgroundColor: 'lightgray',
+  },
+  info: {
+    flexDirection: 'row',
+  },
+  eachInfo: {
+    flex: 1,
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+  },
+  acceptButton: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderBottomLeftRadius: 5,
+    borderTopLeftRadius: 5,
+    flex: 1,
+  },
+  rejectButton: {
+    backgroundColor: 'red',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderBottomRightRadius: 5,
+    borderTopRightRadius: 5,
+    flex: 1,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default EachOrder;
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,81 +1,24 @@
 import React, {useCallback} from 'react';
-import {FlatList, Pressable, StyleSheet, Text, View} from 'react-native';
+import {FlatList} from 'react-native';
 import {useSelector} from 'react-redux';
+import EachOrder from '../components/EachOrder';
 import {Order} from '../slices/order';
 import {RootState} from '../store/reducer';
 
 function Orders() {
   const orders = useSelector((state: RootState) => state.order.orders);
 
-  const toggleDetail = useCallback(() => {}, []);
-
   const renderItem = useCallback(({item}: {item: Order}) => {
-    return (
-      <View style={styles.orderContainer}>
-        <Pressable onPress={toggleDetail} style={styles.info}>
-          <Text style={styles.eachInfo}>
-            {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
-          </Text>
-        </Pressable>
-      </View>
-    );
+    return <EachOrder item={item} />;
   }, []);
 
   return (
     <FlatList
       data={orders}
       keyExtractor={item => item.orderId}
-      renderItem={renderItem}>
-      {orders.map(item => (
-        <View style={styles.orderContainer}>
-          <Pressable onPress={toggleDetail} style={styles.info}>
-            <Text style={styles.eachInfo}>
-              {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
-            </Text>
-          </Pressable>
-        </View>
-      ))}
-    </FlatList>
+      renderItem={renderItem}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  orderContainer: {
-    borderRadius: 5,
-    margin: 5,
-    padding: 10,
-    backgroundColor: 'lightgray',
-  },
-  info: {
-    flexDirection: 'row',
-  },
-  eachInfo: {
-    flex: 1,
-  },
-  buttonWrapper: {
-    flexDirection: 'row',
-  },
-  acceptButton: {
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    paddingVertical: 10,
-    borderBottomLeftRadius: 5,
-    borderTopLeftRadius: 5,
-    flex: 1,
-  },
-  rejectButton: {
-    backgroundColor: 'red',
-    alignItems: 'center',
-    paddingVertical: 10,
-    borderBottomRightRadius: 5,
-    borderTopRightRadius: 5,
-    flex: 1,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-});
 
 export default Orders;
